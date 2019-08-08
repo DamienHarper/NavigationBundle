d0322b00dc461992bd634762bace707dcac3695d
Damien Harper <damien.harper@gmail.com>
2019-08-09 01:14:54 +0200

    Very first tests

---
+++ tests/NavigationBundle/DHNavigationBundleTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace DH\NavigationBundle\Tests;
+
+use DH\NavigationBundle\DependencyInjection\DHNavigationExtension;
+use DH\NavigationBundle\DHNavigationBundle;
+use DH\NavigationBundle\NavigationManager;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+/**
+ * @covers \DH\NavigationBundle\DependencyInjection\Configuration
+ * @covers \DH\NavigationBundle\DependencyInjection\Compiler\AddProvidersPass
+ * @covers \DH\NavigationBundle\DependencyInjection\Compiler\FactoryValidatorPass
+ * @covers \DH\NavigationBundle\DependencyInjection\DHNavigationExtension
+ * @covers \DH\NavigationBundle\DHNavigationBundle
+ * @covers \DH\NavigationBundle\NavigationManager
+ * @covers \DH\NavigationBundle\Provider\ProviderAggregator
+ */
+class DHNavigationBundleTest extends TestCase
+{
+    /**
+     * @throws \Exception
+     */
+    public function testDefaultBuild(): void
+    {
+        $container = new ContainerBuilder();
+
+        $bundle = new DHNavigationBundle();
+        $bundle->build($container);
+
+        $extension = new DHNavigationExtension();
+        $extension->load([], $container);
+
+//        $container->set('entity_manager', $em);
+//        $container->setAlias('doctrine.orm.default_entity_manager', 'entity_manager');
+
+        $container->compile();
+
+        $manager = $container->get('dh_navigation.manager');
+        $this->assertInstanceOf(NavigationManager::class, $manager);
+    }
+}
---
+++ tests/NavigationBundle/NavigationManagerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace DH\NavigationBundle\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class NavigationManagerTest extends TestCase
+{
+    public function testUsing(): void
+    {
+        $this->markTestSkipped();
+    }
+}
